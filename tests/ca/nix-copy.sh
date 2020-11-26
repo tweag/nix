eb61c7c764120c6084174afd96da0e8944def272
regnat <rg@regnat.ovh>
2020-11-26 11:37:54 +0100

    Test the signing of drv outputs

--- tests/ca/nix-copy.sh
+++ tests/ca/nix-copy.sh
@@ -3,9 +3,13 @@ source common.sh
 clearStore
 clearCache
 
+nix-store --generate-binary-cache-key cache1.example.org $TEST_ROOT/sk1 $TEST_ROOT/pk1
+pk1=$(cat $TEST_ROOT/pk1)
+
 commonArgs=( \
     --experimental-features 'nix-command flakes ca-derivations ca-references' \
     --file ./content-addressed.nix \
+    --secret-key-files $TEST_ROOT/sk1 \
     -v
 )
 
@@ -19,14 +23,14 @@ clearRemote() {
 backAndForth () {
     # Fill the remote cache (by copy-ing only the toplevel derivation outputs to
     # make sure that the dependencies are properly registered)
-    nix copy --to $remoteRoot --no-require-sigs transitivelyDependentCA dependentNonCA dependentFixedOutput "${commonArgs[@]}"
+    nix copy --to $remoteRoot transitivelyDependentCA dependentNonCA dependentFixedOutput "${commonArgs[@]}"
     clearStore
 
     # Fetch the otput from the cache
     # First one derivation randomly choosen in dependency graph
-    nix copy --from $remoteRoot --no-require-sigs "${commonArgs[@]}" dependentCA
+    nix copy --from $remoteRoot "${commonArgs[@]}" dependentCA
     # Then everything
-    nix copy --from $remoteRoot --no-require-sigs "${commonArgs[@]}"
+    nix copy --from $remoteRoot "${commonArgs[@]}"
 
     # Ensure that everything is locally present
     nix build "${commonArgs[@]}" -j0 --no-link
--- tests/ca/substitute.sh
+++ tests/ca/substitute.sh
@@ -9,8 +9,18 @@ chmod -R u+w "$REMOTE_STORE_PATH" || true
 rm -rf "$REMOTE_STORE_PATH"
 export REMOTE_STORE=file://$REMOTE_STORE_PATH
 
+nix-store --generate-binary-cache-key cache1.example.org $TEST_ROOT/sk1 $TEST_ROOT/pk1
+pk1=$(cat $TEST_ROOT/pk1)
+sk1=$TEST_ROOT/sk1
+
 buildDrvs () {
-    nix --experimental-features 'nix-command ca-derivations ca-references' build --file ./content-addressed.nix -L --no-link "$@"
+    nix \
+        --experimental-features 'nix-command ca-derivations ca-references' \
+        build \
+        --file ./content-addressed.nix \
+        -L --no-link \
+        --secret-key-files $sk1 \
+        "$@"
 }
 
 clearStore
