5e72982dbaa36dc71ec8799aa7654fbfb6be8ef9
Yves-Stan Le Cornec <yves.stan.lecornec@tweag.io>
2024-01-22 13:25:41 +0100

    Disable tests against the nix version from Unstable

--- flake.nix
+++ flake.nix
@@ -704,11 +704,15 @@
           let pkgs = nixpkgsFor.${system}.native; in
           pkgs.runCommand "install-tests" {
             againstSelf = testNixVersions pkgs pkgs.nix pkgs.pkgs.nix;
-            againstCurrentUnstable =
-              # FIXME: temporarily disable this on macOS because of #3605.
-              if system == "x86_64-linux"
-              then testNixVersions pkgs pkgs.nix pkgs.nixUnstable
-              else null;
+
+            # Disabled because the lastest version from unstable does not handle
+            # the GetAccessStatus and SetAccessStatus protocol commands
+            # againstCurrentUnstable =
+            #   # FIXME: temporarily disable this on macOS because of #3605.
+            #   if system == "x86_64-linux"
+            #   then testNixVersions pkgs pkgs.nix pkgs.nixUnstable
+            #   else null;
+
             # Disabled because the latest stable version doesn't handle
             # `NIX_DAEMON_SOCKET_PATH` which is required for the tests to work
             # againstLatestStable = testNixVersions pkgs pkgs.nix pkgs.nixStable;
